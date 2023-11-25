017401acba64671f558942f571317200ced140e6
TheTuberBee <denes999x@gmail.com>
2023-11-25 06:13:41 +0100

    calendar demo endpoint

--- routers/debug.py
+++ routers/debug.py
@@ -2,8 +2,13 @@ from fastapi import APIRouter, Response, Request, HTTPException, status
 
 from models import *
 
+from common.calendar import get_calendar
+
 
 router = APIRouter(tags=["debug"])
 
 # here should go wrappers of common functions that should be tested
 
+@router.get("/calendar")
+async def calendar():
+    return await get_calendar()
