b78d9f7107d09577d0d2cf65ba7a58f926fd57ed
Daniel Gergely <daniel.gergely.2005@gmail.com>
2023-11-26 09:52:29 +0100

    cors fuck you

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000", "https://junctionx-2023-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
